56d466a264ccb17751005cb1a50f9211971cd3e3
lupis <kd3180@hotmail.com>
2014-11-02 18:43:07 +0100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,134 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Watch for changes and trigger compass and livereload
+    // https://github.com/gruntjs/grunt-contrib-watch
+    watch: {
+      compass: {
+        files: ['sass/{,**/}*.scss'],
+        tasks: ['compass:dev']
+      },
+      includes: {
+        files: ['include/**/*', 'index.html'],
+        tasks: ['includes']
+      },
+      
+      livereload: {
+        options: {
+          livereload: 1337
+        },
+        files: [
+          'build/css/style.css',
+          'build/*.html',
+          'build/styleguide/*.html',
+          'js/*.js',
+          'images/{,**/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // Grunt connect
+    // https://github.com/gruntjs/grunt-contrib-connect
+    connect: {
+      server: {
+       options: {
+        livereload: 1337,
+        port: 9001,
+        base: 'build/',
+        open: {
+          target: 'http://localhost:9001/',
+        }
+       }
+     }
+    },
+
+    // Grunt Includes
+    // https://www.npmjs.org/package/grunt-includes
+    includes: {
+      files: {
+       src: ['*.html'], // Source files
+       dest: 'build', // Destination directory
+       cwd: '.',
+       options: {
+        includePath: 'include'
+       }
+     }
+    },
+
+    // Grunt Contrib Compass
+    // https://github.com/gruntjs/grunt-contrib-compass
+    compass: {
+      options: {
+        sassDir: 'sass',
+        cssDir: 'build/css',
+        bundleExec: true,
+        require: ['sass-globbing', 'susy'],
+      },
+      dev: {
+        options: {
+          environment: 'development',
+          outputStyle: 'expanded',
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          outputStyle: 'compact',
+        }
+      },
+    },
+
+    bower: {
+      install: {
+        options: {
+          targetDir: './lib'
+        }
+      }
+    },
+
+    
+
+    shell: {
+      bundler: {
+        command: 'bundle --path lib'
+      }
+    },
+
+    
+
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-bower-task');
+  
+  
+
+  grunt.registerTask('default', [
+    'shell:bundler',
+    'bower',
+    'compass:dev',
+    'includes',
+    'connect',
+    'watch'
+  ]);
+
+};
+
+
+
+
+
+
+
+
+
+
+
